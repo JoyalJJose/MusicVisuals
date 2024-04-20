bf05a258b3cd6c7f7f10b761c9eec1ee65dd7a3c
JoyalJJose <C22756115@mytudublin.ie>
2024-04-20 21:43:38 +0100

    Setting up repo

---
+++ java/src/C22756115/JoyalsVisual.java
@@ -0,0 +1,5 @@
+package C22756115;
+
+public class JoyalsVisual extends ie.tudublin.Visual {
+    
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,12 +3,14 @@ package ie.tudublin;
 import example.CubeVisual;
 import example.MyVisual;
 import example.RotatingAudioBands;
+import C22756115.JoyalsVisual;
 
 public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
         processing.core.PApplet.runSketch(a, new MyVisual());
+        // processing.core.PApplet.runSketch(a, new JoyalsVisual());
     }
 
     public static void main(String[] args) {
