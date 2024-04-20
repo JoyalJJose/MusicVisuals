958eba86c0c5e9eb2a9301ee6834e00a03692eb6
JoyalJJose <C22756115@mytudublin.ie>
2024-04-20 22:02:19 +0100

    intialise settings()

--- java/src/C22756115/JoyalsVisual.java
+++ java/src/C22756115/JoyalsVisual.java
@@ -1,5 +1,21 @@
 package C22756115;
 
 public class JoyalsVisual extends ie.tudublin.Visual {
+
+    public void settings()
+    {
+        size(1024, 1000, P3D);
+        // fullScreen(P3D, SPAN);
+    }
+
+    public void setup()
+    {
+
+    }
+
+    public void draw()
+    {
+
+    }
     
 }
